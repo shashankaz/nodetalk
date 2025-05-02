fe132d77c92c549ed1250e909664d4765ebd0b02
Shashank <113260615+shashankaz@users.noreply.github.com>
2025-05-02 19:20:44 +0530

    Change userData export to a constant in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ const inter = Inter({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
-export const userData = [
+const userData = [
   {
     name: "John Doe",
     avatar: "https://randomuser.me/api/portraits/men/1.jpg",
